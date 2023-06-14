32960914e74f8fee1f1b2debeb31397b6369aef8
Cuadri84 <davidcuadrillerosaura@gmail.com>
2023-06-14 19:22:26 +0200

    erasing some lines of inecesary code

--- index.test.js
+++ index.test.js
@@ -1,43 +1,53 @@
 const { Room, Booking } = require("./index");
 
-const booking1 = new Booking({
-  name: "John Doe",
-  email: "johndoe@example.com",
-  checkIn: new Date("2023-06-01"),
-  checkOut: new Date("2023-06-02"),
-  discount: 10,
-});
-
-const booking2 = new Booking({
-  name: "Jane Smith",
-  email: "janesmith@example.com",
-  checkIn: new Date("2023-07-15"),
-  checkOut: new Date("2023-07-25"),
-  discount: 0,
-});
-
-const room1 = {
-  name: "Standard",
-  rate: 100,
-  discount: 0,
-};
-
 describe("if room is occupied method", () => {
   it("booking fo that room", () => {
-    const room = new Room(room1);
-    room.addBooking(booking1).addBooking(booking2);
+    const room = new Room({
+      name: "Standard",
+      rate: 100,
+      discount: 0,
+    });
+    room
+      .addBooking({
+        name: "John Doe",
+        email: "johndoe@example.com",
+        checkIn: new Date("2023-06-01"),
+        checkOut: new Date("2023-06-02"),
+        discount: 10,
+      })
+      .addBooking({
+        name: "Jane Smith",
+        email: "janesmith@example.com",
+        checkIn: new Date("2023-07-15"),
+        checkOut: new Date("2023-07-25"),
+        discount: 0,
+      });
     expect(room.isOccupied(new Date("2023-07-17"))).toBe(true);
   });
   it("not booking for that room", () => {
-    const room = new Room(room1);
-    room.addBooking(booking1);
+    const room = new Room({
+      name: "Standard",
+      rate: 100,
+      discount: 0,
+    });
+    room.addBooking({
+      name: "John Doe",
+      email: "johndoe@example.com",
+      checkIn: new Date("2023-06-01"),
+      checkOut: new Date("2023-06-02"),
+      discount: 10,
+    });
     expect(room.isOccupied(new Date("2023-09-08"))).toBe(false);
   });
 });
 
 describe("get fee method", () => {
   it("0 discount", () => {
-    const room3 = new Room(room1);
+    const room3 = new Room({
+      name: "Standard",
+      rate: 100,
+      discount: 0,
+    });
     const booking = new Booking({
       name: "John Doe",
       email: "johndoe@example.com",
@@ -97,6 +107,7 @@ describe("get fee method", () => {
     expect(booking.getFee()).toBe(100);
   });
 });
+
 describe("returns percentage of days with ocupancy within the range of days given method", () => {
   it("0% occupancy", () => {
     const room = new Room({
